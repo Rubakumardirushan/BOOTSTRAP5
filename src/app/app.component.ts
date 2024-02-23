a4eb5c1cd706a1ff62f49e709555b6b74a531131
Rubakumardirushan <dirusha-se19033@stu.kln.ac.lk>
2024-02-23 16:10:58 +0530

    Find Github User

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LearningComponent } from './Bootstrap/learning/learning.component';
+import { FindUserComponent } from './GitHub/find-user/find-user.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,LearningComponent],
+  imports: [RouterOutlet,LearningComponent,FindUserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
