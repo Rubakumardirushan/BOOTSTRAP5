b38eb6267ba2733abc4b4fc0845687fccc4a6341
Rubakumardirushan <dirusha-se19033@stu.kln.ac.lk>
2024-02-23 10:32:05 +0530

    Bootstrap5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { LearningComponent } from './Bootstrap/learning/learning.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LearningComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
