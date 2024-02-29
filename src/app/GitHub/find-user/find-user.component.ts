ec3084c051ccce9574a4cd32d6ee518513de033e
Rubakumardirushan <dirusha-se19033@stu.kln.ac.lk>
2024-02-29 11:40:11 +0530

    start to learn Bootstrap 5

--- src/app/GitHub/find-user/find-user.component.ts
+++ src/app/GitHub/find-user/find-user.component.ts
@@ -13,7 +13,6 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './find-user.component.css',
 })
 export class FindUserComponent implements OnInit {
-
   constructor(private http: HttpClient) {}
   username: string = '';
 
@@ -23,42 +22,35 @@ export class FindUserComponent implements OnInit {
 
   userinfo = new userdeatils();
   findname() {
-    this.userinfo.follower=[];
-    this.userinfo.conut=false;
+    this.userinfo.follower = [];
+    this.userinfo.conut = false;
     this.http
       .get('https://api.github.com/users/' + this.username)
       .subscribe((result: any) => {
         this.userinfo.bio = result['bio'];
         this.userinfo.avatar_url = result['avatar_url'];
-
+        console.log(result);
         this.userinfo.followers_url = result['followers_url'];
         this.userinfo.following_url = result['following_url'];
         console.log(this.userinfo.following_url);
         this.userinfo.name = result['name'];
         this.userinfo.location = result['location'];
         this.userinfo.public_repos = result['public_repos'];
+        if (!result) {
+          alert('User not found');
+        }
       });
   }
-ngOnInit(): void {
-
-
-}
-getfollowers(){
-this.userinfo.conut=true;
-  this.http.get(this.userinfo.followers_url).subscribe((followers:any)=>{
-for(let i=0;i<followers.length;i++){
-    this.userinfo.follower.push(followers[i].login);
-    console.log(followers[i].login);
-}
-  })
-}
-
-
-
-
-
-
-
+  ngOnInit(): void {}
+  getfollowers() {
+    this.userinfo.conut = true;
+    this.http.get(this.userinfo.followers_url).subscribe((followers: any) => {
+      for (let i = 0; i < followers.length; i++) {
+        this.userinfo.follower.push(followers[i].login);
+        console.log(followers[i].login);
+      }
+    });
+  }
 }
 export class userdeatils {
   bio: string = '';
@@ -68,7 +60,7 @@ export class userdeatils {
   name: string = '';
   location: string = '';
   public_repos: number = 0;
-  follower:string[]=[];
-  following:string[]=[];
-  conut:boolean=false;
+  follower: string[] = [];
+  following: string[] = [];
+  conut: boolean = false;
 }
